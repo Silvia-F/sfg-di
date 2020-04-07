5ac06d9acd9f8f186828a2c5501130a573ed4dca
Silvia-F <Silvia-F@users.noreply.github.com>
2020-04-07 13:24:10 +0100

    Add assignment behaviour

--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -1,14 +1,17 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
+/**
+ * Created by jt on 12/27/19.
+ */
 @Primary
-@Controller
+@Service
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello World - Primary";
+        return "Hello World - From the PRIMARY Bean";
     }
 }
