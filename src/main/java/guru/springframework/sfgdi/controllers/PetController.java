ba0ba1e6cee539a1e1b516e16202952b199f67f9
Silvia-F <Silvia-F@users.noreply.github.com>
2020-04-07 13:33:15 +0100

    Clean up

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -3,9 +3,6 @@ package guru.springframework.sfgdi.controllers;
 import guru.springframework.sfgdi.services.PetService;
 import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 12/28/19.
- */
 @Controller
 public class PetController {
 
